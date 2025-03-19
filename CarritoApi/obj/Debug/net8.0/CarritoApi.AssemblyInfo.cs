07e8602a5f85a11e5213f3e45991040cc78d8c5f
Pablo Bazan <bazanpabloivan@gmail.com>
2025-03-19 11:46:10 -0300

    CarritoEstado

--- CarritoApi/obj/Debug/net8.0/CarritoApi.AssemblyInfo.cs
+++ CarritoApi/obj/Debug/net8.0/CarritoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarritoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2385f4702a1f4cf0b85ea75f42d435bc780ebbd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38b0aed5ceeb04e6ca4713ba1bfc2d8477b8388a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarritoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarritoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
